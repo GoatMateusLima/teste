64357e2187b7921f33fc88c08340751c25b843c9
MateusLima <165662012+GoatMateusLima@users.noreply.github.com>
2024-09-08 10:23:06 -0300

    primeiro commit

---
+++ Hello.py
@@ -0,0 +1,12 @@
+nome = "mateus"
+print("Ola mundo, meu nome é:", (nome))
+
+altura = "1.80" 
+idade = "17 anos"
+sexo = "masculino"
+
+
+print("altura:", (altura))
+print("idade:", (idade))
+print("sexo:", (sexo))
+
---
+++ controle de fluxo.py
@@ -0,0 +1,6 @@
+idade = 18
+
+if idade >= 18:
+    print("Voce é adulto.")
+else:
+    print("voce é menor de idade.")
